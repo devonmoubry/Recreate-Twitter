6f8d61f920ce161812748313c05286c0cfff84a2
Devon Moubry <devon@moubry.com>
2017-04-10 20:03:52 -0500

    add edit delete buttons to user tweets

--- app/scripts/views/tweet_view.js
+++ app/scripts/views/tweet_view.js
@@ -5,19 +5,26 @@ export default function ( store ) {
   //edit and delete dropdown conditional upon ownership
   var $twitterFeed = $('<ul class=""><ul>');
   var tweets = store.getState().tweets;
+
   tweets.forEach( function( tweet, index, array ) {
-    $twitterFeed.append($(`
+    var $tweetView = $(`
       <li class="tweet-view">
         <i class="fa fa-user-circle-o" aria-hidden="true"></i>
         <p class="username">${tweet.user.name}</p>
         <p class="tweet-text">${tweet.tweet}</p>
         <p class="timestamp">${tweet.created}</p>
-        <section class="edit-delete" id="edit-delete">
-          <p>Edit</p>
-          <p>Delete</p>
-        </section>
       </li>
-    `));
-  })
+      `);
+
+      if (tweet.user.name === 'frank medrano') {
+            $tweetView.append($(`
+              <section class="edit-delete" id="edit-delete">
+                <p>Edit</p>
+                <p>Delete</p>
+              </section>`));
+        }
+
+        $twitterFeed.append($tweetView);
+  });
   return $twitterFeed;
 }
